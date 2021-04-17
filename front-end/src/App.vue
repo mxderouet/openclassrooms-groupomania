82eed062d37dd4096ae0efcdf281280ba20a946d
maxime <mxderouet@gmail.com>
2021-04-17 16:05:42 +0200

    header component

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
     <Header></Header>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>
-      <test></test>
-    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import test from './components/test.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    test
+    Header
   }
 }
 </script>
---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,21 @@
+// ./components/Header.vue
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<template>
+    <div>
+        <img src="../assets/logo-groupomania.png" alt="logo groupomania" id="logo">
+    </div>  
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+    #logo {
+        height: 15em;
+    }
+</style>
